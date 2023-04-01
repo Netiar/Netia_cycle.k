696db3708384adbf4113eb0dd87f3727364c4cfa
Netiar <collimow54@gmail.com>
2023-04-02 01:03:36 +0300

    add functions for add-event and on-cleared

--- app/src/main/java/com/example/netia_cyclek/viewmodel/NetiaViewModel.kt
+++ app/src/main/java/com/example/netia_cyclek/viewmodel/NetiaViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.netia_cyclek.viewmodel
 
-class NetiaViewModel {
+import android.os.SystemClock
+import androidx.lifecycle.ViewModel
+import com.example.netia_cyclek.models.Netia
+import java.util.*
+
+class NetiaViewModel : ViewModel() {
+    val netia: MutableList<Netia> = mutableListOf()
+    val startTime = SystemClock.elapsedRealtime()
+    private val id = Random().nextInt()
+
+    fun addEvent(message: String, activityHash: Int) {
+        netia.add(Netia(message, activityHash, id))
+    }
+
+    override fun onCleared() {
+        netia.clear()
+    }
 }
