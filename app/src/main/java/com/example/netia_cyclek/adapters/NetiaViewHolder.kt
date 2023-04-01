ed6bed03d6e0f541ab9b06eab9ba42dc18672bb1
Netiar <collimow54@gmail.com>
2023-04-02 01:08:35 +0300

    add bind-to function

--- app/src/main/java/com/example/netia_cyclek/adapters/NetiaViewHolder.kt
+++ app/src/main/java/com/example/netia_cyclek/adapters/NetiaViewHolder.kt
@@ -1,4 +1,20 @@
 package com.example.netia_cyclek.adapters
 
-class NetiaViewHolder {
+import android.text.format.DateUtils
+import androidx.recyclerview.widget.RecyclerView
+import com.example.netia_cyclek.databinding.NetiaBinding
+import com.example.netia_cyclek.models.Netia
+
+class NetiaViewHolder(
+    private val binding: NetiaBinding,
+    private val startTime: Long
+) : RecyclerView.ViewHolder(binding.root)  {
+    fun bindTo(netia: Netia) {
+        val elapsedSeconds = (netia.timestamp - startTime) / 1000
+
+        binding.timestamp.text = DateUtils.formatElapsedTime(elapsedSeconds)
+        binding.message.text = netia.message
+        binding.activityHash.text = Integer.toHexString(netia.activityHash)
+        binding.viewHash.text = Integer.toHexString(netia.viewHash)
+    }
 }
