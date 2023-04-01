50c438a290157a53537342d8fc8fd71de10df840
Netiar <collimow54@gmail.com>
2023-04-02 01:11:16 +0300

    add onCreatViewHolder, onBindViewHolder and a NetiaDiffer function constructors

--- app/src/main/java/com/example/netia_cyclek/adapters/NetiaAdapter.kt
+++ app/src/main/java/com/example/netia_cyclek/adapters/NetiaAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.netia_cyclek.adapters
 
-class NetiaAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.netia_cyclek.databinding.NetiaBinding
+import com.example.netia_cyclek.models.Netia
+
+internal class NetiaAdapter(
+    private val inflater: LayoutInflater,
+    private val startTime: Long
+) : ListAdapter<Netia, NetiaViewHolder>(NetiaDiffer) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ) = NetiaViewHolder(NetiaBinding.inflate(inflater, parent, false), startTime)
+
+
+    override fun onBindViewHolder(holder: NetiaViewHolder, position: Int) {
+        holder.bindTo(getItem(position))
+    }
+
+    private object NetiaDiffer : DiffUtil.ItemCallback<Netia>() {
+        override fun areItemsTheSame(oldEvent: Netia, newEvent: Netia) =
+            oldEvent === newEvent
+
+        override fun areContentsTheSame(oldEvent: Netia, newEvent: Netia) =
+            oldEvent == newEvent
+    }
+
 }
