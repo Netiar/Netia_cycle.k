318c710a429a75bb260870869afa940a1e13d437
Netiar <collimow54@gmail.com>
2023-04-02 01:12:30 +0300

    add main activity binding and on life-cycle methods

--- app/src/main/java/com/example/netia_cyclek/MainActivity.kt
+++ app/src/main/java/com/example/netia_cyclek/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.netia_cyclek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.netia_cyclek.adapters.NetiaAdapter
+import com.example.netia_cyclek.databinding.ActivityMainBinding
+import com.example.netia_cyclek.viewmodel.NetiaViewModel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adp: NetiaAdapter
+    private val vm: NetiaViewModel by viewModels()
+    private val id = Random().nextInt()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        adp = NetiaAdapter(layoutInflater, vm.startTime)
+        addEvent("onCreate()")
+
+        binding.rec.layoutManager = LinearLayoutManager(this)
+        binding.rec.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+        binding.rec.adapter = adp
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        addEvent("onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        addEvent("onResume()")
+    }
+
+    override fun onPause() {
+        addEvent("onPause()")
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        addEvent("onStop()")
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        addEvent("onDestroy()")
+
+        super.onDestroy()
+    }
+
+    private fun addEvent(message: String) {
+        vm.addEvent(message, id)
+        adp.submitList(ArrayList(vm.netia))
     }
 }
