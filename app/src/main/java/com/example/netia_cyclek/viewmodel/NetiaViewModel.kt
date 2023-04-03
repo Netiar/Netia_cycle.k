f0705451c0bf99be635187ffed04e634f3e95c74
Netiar <collimow54@gmail.com>
2023-04-03 17:02:09 +0300

    add saved instance state

--- app/src/main/java/com/example/netia_cyclek/viewmodel/NetiaViewModel.kt
+++ app/src/main/java/com/example/netia_cyclek/viewmodel/NetiaViewModel.kt
@@ -1,17 +1,25 @@
 package com.example.netia_cyclek.viewmodel
 
 import android.os.SystemClock
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.netia_cyclek.models.Netia
 import java.util.*
 
-class NetiaViewModel : ViewModel() {
-    val netia: MutableList<Netia> = mutableListOf()
-    val startTime = SystemClock.elapsedRealtime()
+private const val STATE_NETIA = "netia"
+private const val STATE_START_TIME = "startTime"
+
+
+class NetiaViewModel(private val state: SavedStateHandle) : ViewModel(){
+
+    val netia: ArrayList<Netia> = state[STATE_NETIA] ?: arrayListOf()
+    val startTime: Long = state[STATE_START_TIME]
+        ?: SystemClock.elapsedRealtime().also { state[STATE_START_TIME] = it }
     private val id = Random().nextInt()
 
     fun addEvent(message: String, activityHash: Int) {
         netia.add(Netia(message, activityHash, id))
+        state[STATE_NETIA] = netia
     }
 
     override fun onCleared() {
