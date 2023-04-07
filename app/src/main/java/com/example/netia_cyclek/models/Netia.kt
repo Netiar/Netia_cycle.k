d813b7be7b96ed254d1a0bc8039d4f3efb2726df
Netiar <collimow54@gmail.com>
2023-04-07 17:49:35 +0300

    ft: make data class Parcelable

--- app/src/main/java/com/example/netia_cyclek/models/Netia.kt
+++ app/src/main/java/com/example/netia_cyclek/models/Netia.kt
@@ -1,10 +1,13 @@
 package com.example.netia_cyclek.models
 
+import android.os.Parcelable
 import android.os.SystemClock
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Netia(
     val message: String,
     val activityHash: Int,
     val viewHash: Int,
     val timestamp: Long = SystemClock.elapsedRealtime()
-)
+) : Parcelable
